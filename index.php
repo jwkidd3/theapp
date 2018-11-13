584289eabbf503cabdac5330fa922cbf6a7a0399
jwkidd3 <jkidd@kiddcorp.com>
2018-11-13 06:09:49 -0600

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Awesome app</title>
+  </head>
+  <body>
+  <h1>Welcome to your awesome app !</h1>
+
+  <p> App backend <?=gethostname()?> </p>
+
+    <img src="http://fillmurray.com/408/287" />
+  </body>
+</html>
+
+
