a0d57a766058723628ec23e6382d21b68b9af121
Mr. J. Detert <jon.detert@gmail.com>
2018-11-13 13:24:50 -0600

    add image and reference to caddy shack
    i think this adds a certain flare on an otherwise awesome app

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
   <p> App backend <?=gethostname()?> </p>
 
     <img src="http://fillmurray.com/408/287" />
+  <p>So we have that going for us.<p>
+  <img src="http://application.denofgeek.com/images/m/murray/008_Caddyshack.jpg" />
+  <p>which is nice</p>
   </body>
 </html>
 
